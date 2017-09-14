f492d765c908bf20a20879364b1952ae06013fef
hupochuan <1434296509@qq.com>
2017-09-14 15:33:53 +0800

    model建立
    website：提供rss服务的网站；RssItemBean：文章

---
+++ src/com/ycl/bean/RSSItemBean.java
@@ -0,0 +1,16 @@
+package com.ycl.bean;
+
+import java.util.Date;
+
+public class RSSItemBean {
+	private String title;
+    private String author;
+    private String uri;
+    private String link;
+    private String description;
+    private Date pubDate;
+    private String type;
+    private String content;
+    private int rssid;
+
+}
---
+++ src/com/ycl/bean/Website.java
@@ -0,0 +1,76 @@
+package com.ycl.bean;
+
+public class Website {
+	
+	private String title;
+	private String url;
+	private String startTag;
+	private String endTag;
+	private String encoding;
+	private String rssid;
+	private int interval;
+	private String theme;
+	private Boolean isenabled;
+	private Boolean theme_chooseable;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getStartTag() {
+		return startTag;
+	}
+	public void setStartTag(String startTag) {
+		this.startTag = startTag;
+	}
+	public String getEndTag() {
+		return endTag;
+	}
+	public void setEndTag(String endTag) {
+		this.endTag = endTag;
+	}
+	public String getEncoding() {
+		return encoding;
+	}
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+	public String getRssid() {
+		return rssid;
+	}
+	public void setRssid(String rssid) {
+		this.rssid = rssid;
+	}
+	public int getInterval() {
+		return interval;
+	}
+	public void setInterval(int interval) {
+		this.interval = interval;
+	}
+	public String getTheme() {
+		return theme;
+	}
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+	public Boolean getIsenabled() {
+		return isenabled;
+	}
+	public void setIsenabled(Boolean isenabled) {
+		this.isenabled = isenabled;
+	}
+	public Boolean getTheme_chooseable() {
+		return theme_chooseable;
+	}
+	public void setTheme_chooseable(Boolean theme_chooseable) {
+		this.theme_chooseable = theme_chooseable;
+	}
+}
